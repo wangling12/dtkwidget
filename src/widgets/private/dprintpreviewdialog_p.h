c24736d7ddf71e33e497cca7b1353cd563a1fa42
kongyunzhen <kongyunzhen@uniontech.com>
2020-07-16 22:34:57 +0800

    feat: ui主题框架创建，左侧控件添加布局调整
    Description: ui主题框架创建，左侧控件添加布局调整
    
    Log: 无

--- src/widgets/private/dprintpreviewdialog_p.h
+++ src/widgets/private/dprintpreviewdialog_p.h
@@ -3,14 +3,31 @@
 
 #include "dprintpreviewdialog.h"
 #include "ddialog_p.h"
-
+class QVBoxLayout;
+class QPushButton;
 DWIDGET_BEGIN_NAMESPACE
-
+class DFrame;
+class DIconButton;
+class DLineEdit;
+class DLabel;
 class DPrintPreviewDialogPrivate : public DDialogPrivate
 {
 public:
     explicit DPrintPreviewDialogPrivate(DPrintPreviewDialog *qq);
+    void initui();
+    void initleft(QVBoxLayout *layout);
+    void initright(QVBoxLayout *layout);
 
+
+    DFrame *pview;
+    DIconButton *firstBtn;
+    DIconButton *prevPageBtn;
+    DLineEdit *jumpPageEdit;
+    DLabel *totalPageLabel;
+    DIconButton *nextPageBtn;
+    DIconButton *lastBtn;
+    QPushButton *cancelBtn;
+    QPushButton *printBtn;
     Q_DECLARE_PUBLIC(DPrintPreviewDialog)
 };
 
