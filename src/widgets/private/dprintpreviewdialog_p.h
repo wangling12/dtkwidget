d12363e0437ad58657e9119331c57ef6bc956e1e
kongyunzhen <kongyunzhen@uniontech.com>
2020-07-28 15:28:09 +0800

    fix: 将打印预览对外公有槽改为私有
    Description: 将dprintpreviewdialog中的公有槽函数调整为私有槽函数
    
    Log: 调整打印预览对外接口
    Change-Id: Idc6ea6f238381d617ff23508027e64c645d32652
    Reviewed-on: http://gerrit.uniontech.com/c/dtkwidget/+/768
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: chenke <chenke@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/widgets/private/dprintpreviewdialog_p.h
+++ src/widgets/private/dprintpreviewdialog_p.h
@@ -65,6 +65,10 @@ public:
 
 
     void setEnable(const int &value, DComboBox *combox);//控件可用
+    void _q_printerChanged(int index);
+    void _q_pageRangeChanged(int index);
+    void _q_pageMarginChanged(int index);
+    void _q_startPrint(bool clicked);
 
     //printer
     DPrinter *printer;
