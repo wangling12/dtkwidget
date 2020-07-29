607b9199dbb8a1ad7a2601dafcf68adfb2af28be
chenbin <chenbin@uniontech.com>
2020-07-29 15:18:48 +0800

    feat: 预览色彩选项实现
    选择黑白预览为黑白，选择彩色时预览为彩色
    
    Log: 预览色彩选项效果实现
    Change-Id: I1b1c76f02c9a53441b6efe5314660d3379e80aff
    Reviewed-on: http://gerrit.uniontech.com/c/dtkwidget/+/857
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: chenke <chenke@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/widgets/private/dprintpreviewdialog_p.h
+++ src/widgets/private/dprintpreviewdialog_p.h
@@ -68,6 +68,7 @@ public:
     void _q_printerChanged(int index);
     void _q_pageRangeChanged(int index);
     void _q_pageMarginChanged(int index);
+    void _q_ColorModeChange(int index);
     void _q_startPrint(bool clicked);
 
     //printer
